571bac625b6072aa872f3ee1bde654bf89d7ad17
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2023-10-02 16:22:48 +0300

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-# takes in a URL and displays all HTTP methods the server will accept.
-curl -Is "$1" | grep "Allow:" | cut -d ":" -f 2 | cut -c 2- | rev | cut -c 2- | rev
-=======
-# displays http methods
-curl -sI "$1" -X OPTIONS | grep "Allow" | cut -c 8-
->>>>>>> e514e892f6cc2f6ae220c81aa6f9215bc7fe1247
+#takes in a URL and displays all HTTP methods the server will accept
+curl -sI "$1" | grep "Allow" | cut -d " " -f2-
