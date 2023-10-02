d6a8582918ab270da984804d869a655e04700d4c
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2023-10-02 16:25:30 +0300

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-#  takes in a URL, sends a GET request to the URL, and displays the body of the response
-=======
-# get response but display only body
->>>>>>> e514e892f6cc2f6ae220c81aa6f9215bc7fe1247
-curl -sL "$1"
+# Takes in a URL, sends a GET request to the URL, displays the body of the response
+curl -sX GET "$1" -L 200
