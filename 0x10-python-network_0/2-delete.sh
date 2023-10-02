1d32fb4f3d06fa71a98521d43f72e420c063d9f3
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2023-10-02 17:17:17 +0300

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-# Send a DELETE request to a given URL and display the response body.
-=======
-# use delete request
->>>>>>> e514e892f6cc2f6ae220c81aa6f9215bc7fe1247
+# sends a DELETE request to the URL and displays the body of a response
 curl -sX DELETE "$1"
