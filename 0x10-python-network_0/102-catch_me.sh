1eb890813b8cc2fb360148d73e46e88d98fe2521
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2023-10-02 17:32:40 +0300

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# bash script makes request to certain ip , ctf style 
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin: XSchool" You got me!"user_id=98"
+# Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
