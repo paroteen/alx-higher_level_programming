090560ad97ad99c7993c0852a6f2ffd101b79b26
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2023-10-02 18:08:22 +0300

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,12 @@
 """Fetches https://intranet.hbtn.io/status."""
 import urllib.request
 
+
 if __name__ == "__main__":
     request = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
-        utf8_content = body.decode("utf-8")
-
-        print("Body response:$")
+        print("Body response:")
         print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(repr(body)))
-        print("\t- utf8 content: {}".format(utf8_content))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
