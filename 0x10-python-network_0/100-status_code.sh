5ea94c0375579b1c84dd4fd05f1f702696096aeb
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2023-10-02 17:15:02 +0300

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
-# sends a request to a URL passed as an argument, and displays only the status code of the response. no pipe
-awk 'NR==1{printf "%s", $2}' test7 $(curl -sI "$1" -o test7)
-=======
-# send a request to a url passed as an argument,and displays only the status code of the response
-curl -so /dev/null --write-out "%{http_code}" "$1"
->>>>>>> e514e892f6cc2f6ae220c81aa6f9215bc7fe1247
+<<<<<<< HEAD#!/bin/bash
+# sends a request to a URL passed as an argument and displays only the status code response
+curl -s -o /dev/null -w "%{http_code}" "$1"
