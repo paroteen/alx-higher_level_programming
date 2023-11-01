742baa81fcb4cafee64a6f456c76c94bb48e1fb8
Olivier ITANGISHAKA <codedreamer00@gmail.com>
2023-11-01 14:37:44 -0700

    avascript

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
-
-request(url, function (error, response) {
-  if (error) {
-    console.log(error);
-  }console.log('code:', response.statusCode);
-});
